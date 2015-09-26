5606652f32e8927c919ad90ec81a518976680276
saan800 <sandragreenhalgh@gmail.com>
2015-09-26 22:09:53 +0100

    get appveyor build and tests working

--- build.cmd
+++ build.cmd
@@ -13,14 +13,22 @@ IF NOT EXIST %LocalAppData%\NuGet md %LocalAppData%\NuGet
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest 'https://dist.nuget.org/win-x86-commandline/%NUGET_VERSION%/nuget.exe' -OutFile '%CACHED_NUGET%'"
 
 
-
 :copynuget
-IF EXIST .nuget\nuget.exe goto dnvm
+echo Copy nuget
+IF EXIST .nuget\nuget.exe goto restore
 md .nuget
 copy %CACHED_NUGET% .nuget\nuget.exe > nul
 
 
+:restore
+copy Restore....
+IF EXIST packages\KoreBuild goto run
+.nuget\NuGet.exe install KoreBuild -ExcludeVersion -o packages -nocache -pre
+.nuget\NuGet.exe install Sake -version 0.2 -o packages -ExcludeVersion
+
+
 :dnvm
+copy DNVM....
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "&{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}"
 dnvm install %BUILDCMD_DNX_VERSION%
 dnvm use %BUILDCMD_DNX_VERSION%
