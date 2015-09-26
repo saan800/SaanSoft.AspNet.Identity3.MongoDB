3cdb51e5e37b303ef720b5a03da31a0ecd338fb9
saan800 <sandragreenhalgh@gmail.com>
2015-09-26 22:20:32 +0100

    get appveyor build and tests working

--- build.cmd
+++ build.cmd
@@ -35,6 +35,6 @@ dnvm use %BUILDCMD_DNX_VERSION%
 
 
 :run
-echo Getting ready to run
+echo Getting ready to run....
 CALL packages\KoreBuild\build\dnvm use default -runtime CLR -arch x86
 packages\Sake\tools\Sake.exe -I packages\KoreBuild\build -f makefile.shade %*
